69bf5920f42bbecb0c5aab508addff308e91108a
Tingting Chen <tingtingctt@gmail.com>
2020-04-26 13:32:51 -0700

    added all quiz pages, result page, and local storage of scores

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
 var timeEl = document.querySelector("#time");
 var scoresEl = document.querySelector("#scores");
 var startEl = document.querySelector("#start");
-var quizEl = document.querySelector("#quiz");
+var quizEl = document.querySelector(".quiz");
 var secondsLeft = 75;
+var quizArray = [showQuiz1, showQuiz2, showQuiz3, showQuiz4, showQuiz5, showResult];
+var scores = [];
 
 showStart();
 showTime ();
+initScores()
 
+// -------------- Retrieve Highscores from local storage ------------------------
+
+function initScores() {
+  var storedScores = JSON.parse(localStorage.getItem("scores"));
+  if (storedScores !== null) {
+    scores = storedScores;
+  }
+  console.log(scores);
+}
+
+// ------------------------ Timer Interval ------------------------------------
 
 function setTime() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
     timeEl.textContent = "Time: " + secondsLeft;
 
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
       clearInterval(timerInterval);
-      sendMessage();
+      showResult();
+    }
+    else if (quizEl.getAttribute("id") === "5") {
+      clearInterval(timerInterval);
     }
   }, 1000);
 }
+
+function showTime() {
+  timeEl.textContent = "Time: " + secondsLeft;
+}
+
+function hideTime() {
+  timeEl.innerHTML = "";
+}
+
+//------------------------ Instruction/Start Page ------------------------------------
 
 function showStart() {
   var startH1 = document.createElement("h1");
@@ -36,11 +63,13 @@ function showStart() {
   startBtn.textContent = "Start Quiz";
   startBtn.setAttribute("style", "margin-left: 150px");
   startEl.appendChild(startBtn);
-
 }
 
+//------------------------ Quiz pages-----------------------------------------
+// quizArray[0] - Quiz 1
 function showQuiz1() {
   startEl.innerHTML = "";
+  quizEl.setAttribute ("id", "1");
 
   var quizH2 = document.createElement("h2");
   var lineBreak = document.createElement("br");
@@ -69,9 +98,10 @@ function showQuiz1() {
   quizEl.appendChild(quizBtn4);
 }
 
-
+// quizArray[1] - Quiz 2
 function showQuiz2() {
   quizEl.innerHTML = "";
+  quizEl.setAttribute ("id", "2");
 
   var quizH2 = document.createElement("h2");
   var lineBreak = document.createElement("br");
@@ -100,6 +130,135 @@ function showQuiz2() {
   quizEl.appendChild(quizBtn4);
 }
 
+// quizArray[2] - Quiz 3
+function showQuiz3() {
+  quizEl.innerHTML = "";
+  quizEl.setAttribute ("id", "3");
+
+  var quizH2 = document.createElement("h2");
+  var lineBreak = document.createElement("br");
+  quizH2.textContent = "Arrays in JavaScript can be used to store:";
+  quizEl.appendChild(quizH2);
+  quizEl.appendChild(lineBreak);
+
+  var quizBtn1 = document.createElement("button");
+  quizBtn1.textContent = "1. numbers and strings";
+  quizEl.appendChild(quizBtn1);
+  quizEl.appendChild(lineBreak);
+
+  var quizBtn2 = document.createElement("button");
+  quizBtn2.textContent = "2. other arrays";
+  quizEl.appendChild(quizBtn2);
+  quizEl.appendChild(lineBreak);
+
+  var quizBtn3 = document.createElement("button");
+  quizBtn3.textContent = "3. booleans";
+  quizEl.appendChild(quizBtn3);
+  quizEl.appendChild(lineBreak);
+
+  var quizBtn4 = document.createElement("button");
+  quizBtn4.textContent = "4. all of the above";
+  quizBtn4.setAttribute ("class", "correct");
+  quizEl.appendChild(quizBtn4);
+}
+
+// quizArray[3] - Quiz 4
+function showQuiz4() {
+  quizEl.innerHTML = "";
+  quizEl.setAttribute ("id", "4");
+
+  var quizH2 = document.createElement("h2");
+  var lineBreak = document.createElement("br");
+  quizH2.textContent = "String values must be enclosed within";
+  quizEl.appendChild(quizH2);
+  quizEl.appendChild(lineBreak);
+
+  var quizBtn1 = document.createElement("button");
+  quizBtn1.textContent = "1. commas";
+  quizEl.appendChild(quizBtn1);
+  quizEl.appendChild(lineBreak);
+
+  var quizBtn2 = document.createElement("button");
+  quizBtn2.textContent = "2. curly brackets";
+  quizEl.appendChild(quizBtn2);
+  quizEl.appendChild(lineBreak);
+
+  var quizBtn3 = document.createElement("button");
+  quizBtn3.textContent = "3. quotes";
+  quizEl.appendChild(quizBtn3);
+  quizBtn3.setAttribute ("class", "correct");
+  quizEl.appendChild(lineBreak);
+
+  var quizBtn4 = document.createElement("button");
+  quizBtn4.textContent = "4. parenthesis";
+  quizEl.appendChild(quizBtn4);
+}
+
+// quizArray[4] - Quiz 5
+function showQuiz5() {
+  quizEl.innerHTML = "";
+  quizEl.setAttribute ("id", "5");
+
+  var quizH2 = document.createElement("h2");
+  var lineBreak = document.createElement("br");
+  quizH2.textContent = "A very useful tool used during developement and debugging for printing content to the debugger is:";
+  quizEl.appendChild(quizH2);
+  quizEl.appendChild(lineBreak);
+
+  var quizBtn1 = document.createElement("button");
+  quizBtn1.textContent = "1. JavaScript";
+  quizEl.appendChild(quizBtn1);
+  quizEl.appendChild(lineBreak);
+
+  var quizBtn2 = document.createElement("button");
+  quizBtn2.textContent = "2. terminal/bash";
+  quizEl.appendChild(quizBtn2);
+  quizEl.appendChild(lineBreak);
+
+  var quizBtn3 = document.createElement("button");
+  quizBtn3.textContent = "3. for loops";
+  quizEl.appendChild(quizBtn3);
+  quizEl.appendChild(lineBreak);
+
+  var quizBtn4 = document.createElement("button");
+  quizBtn4.textContent = "4. console log";
+  quizBtn4.setAttribute ("class", "correct");
+  quizEl.appendChild(quizBtn4);
+}
+
+// quizArray[5] - Show Result
+function showResult() {
+  hideTime();
+  quizEl.innerHTML = "";
+  quizEl.setAttribute ("id", "5");
+
+  var quizH2 = document.createElement("h2");
+  var lineBreak = document.createElement("br");
+  quizH2.textContent = "All Done!";
+  quizEl.appendChild(quizH2);
+  quizEl.appendChild(lineBreak);
+
+  var resultP = document.createElement("p");
+  resultP.textContent = "Your final score is " + secondsLeft;
+  quizEl.appendChild(resultP);
+  quizEl.appendChild(lineBreak);
+
+  var form = document.createElement("form");
+  quizEl.appendChild(form);
+  var label = document.createElement("label");
+  label.textContent = "Enter initials: ";
+  form.appendChild(label);
+  var input = document.createElement("input");
+  input.setAttribute("class", "initials")
+  form.appendChild(input);
+  var submitBtn = document.createElement("button");
+  submitBtn.textContent = "Submit";
+  submitBtn.setAttribute("class", "submit")
+  form.appendChild(submitBtn);
+}
+
+// ------------------------
+
 function showScores() {
   scoresEl.setAttribute("display", "block");
 }
@@ -108,14 +267,9 @@ function hideScores() {
   scoresEl.setAttribute("display", "none");
 }
 
-function showTime() {
-  timeEl.textContent = "Time: " + secondsLeft;
-}
 
-function hideTime() {
-  timeEl.innerHTML = "";
-}
 
+// ----------------------- Event Listeners ----------------------------------
 
 startEl.addEventListener("click", function(event) {
   event.preventDefault();
@@ -137,7 +291,15 @@ quizEl.addEventListener("click", function(event) {
     quizEl.appendChild(line);
     quizEl.appendChild(correctMsg);
   }
-  if (answer.matches("button") && answer.getAttribute("class") !== "correct") {
+
+  else if (answer.matches("button") && answer.getAttribute("class") === "submit") {
+    var input = document.querySelector(".initials");
+    scores.push(input.value + "-" + secondsLeft);
+    localStorage.setItem("scores", JSON.stringify(scores));
+    console.log(scores);
+  }
+
+  else if (answer.matches("button") && answer.getAttribute("class") !== "submit" && answer.getAttribute("class") !== "correct") {
     var wrongMsg = document.createElement("p");
     var line = document.createElement("hr");
     wrongMsg.textContent = "Wrong! You lost 10 seconds.";
@@ -145,7 +307,9 @@ quizEl.addEventListener("click", function(event) {
     line.setAttribute ("style", "color: grey");
     quizEl.appendChild(line);
     quizEl.appendChild(wrongMsg);
+    secondsLeft = secondsLeft - 10;
   }
   
-  setTimeout (showQuiz2, 1000);
+  var i = answer.parentElement.getAttribute("id");
+  setTimeout (quizArray[i], 1000);
 });
